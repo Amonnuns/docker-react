e32a259c75b92d195294dda54e070c29342ee5ee
Amonnuns <ramonwx15@gmail.com>
2022-01-25 17:30:33 -0300

    chnaged app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React right now
+          Ramon is a great developer
         </a>
       </header>
     </div>
